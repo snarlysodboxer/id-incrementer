02eee39c258706799e46527ba038eb4a44d88ebe
david amick <github@davidamick.com>
2016-08-11 14:55:01 -0700

    Add setter

--- main.go
+++ main.go
@@ -7,35 +7,61 @@ import (
 	"strconv"
 )
 
-func main() {
-	idMap := map[string]map[string]int{}
-	initialValue := 42
-	incrementBy := 5
+var idMap = map[string]map[string]int{}
+var initialValue = 42
+var incrementBy = 5
 
-	router := gin.Default()
-
-	router.GET("/:environment/:name", func(context *gin.Context) {
-		name := context.Param("name")
-		environment := context.Param("environment")
-		// check if the environment is found, otherwise add it
-		if _, ok := idMap[environment]; ok {
-			// check if the name is found, otherwise add it
-			if id, ok := idMap[environment][name]; ok {
-				// increment id and return it
-				idMap[environment][name] = id + incrementBy
-				context.String(http.StatusOK, fmt.Sprintf("%d", idMap[environment][name]))
+func getOrSetID(name, environment string, passedID int) (int, string) {
+	// check if the environment is found, otherwise add it
+	if _, ok := idMap[environment]; ok {
+		// check if the name is found, otherwise add it
+		if foundID, ok := idMap[environment][name]; ok {
+			if passedID == 0 {
+				idMap[environment][name] = foundID + incrementBy
 			} else {
-				// add unfound name
-				fmt.Printf("Name %s was not found in Environment %s, adding it\n", name, environment)
-				idMap[environment][name] = initialValue
-				context.String(http.StatusOK, strconv.Itoa(initialValue))
+				idMap[environment][name] = passedID
 			}
 		} else {
-			// add unfound environment and name
-			fmt.Printf("Environment %s was not found, adding it\n", environment)
-			idMap[environment] = map[string]int{name: initialValue}
-			context.String(http.StatusOK, strconv.Itoa(initialValue))
+			// add unfound name, return ID
+			fmt.Printf("Name %s was not found in Environment %s, adding it\n", name, environment)
+			if passedID == 0 {
+				idMap[environment][name] = initialValue
+			} else {
+				idMap[environment][name] = passedID
+			}
 		}
+	} else {
+		// add unfound environment and name, return ID
+		fmt.Printf("Environment %s was not found, adding it\n", environment)
+		if passedID == 0 {
+			idMap[environment] = map[string]int{name: initialValue}
+		} else {
+			idMap[environment] = map[string]int{name: passedID}
+		}
+	}
+	return http.StatusOK, strconv.Itoa(idMap[environment][name])
+}
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/setter", func(context *gin.Context) {
+		passedID, err := strconv.Atoi(context.PostForm("id"))
+		if err != nil {
+			context.String(http.StatusBadRequest, fmt.Sprintf("Error converting %s to an integer", context.PostForm("id")))
+			return
+		}
+		if passedID == 0 {
+			context.String(http.StatusBadRequest, "You can't set the ID to zero") // TODO rethink this limitation?
+		} else {
+			status, id := getOrSetID(context.PostForm("name"), context.PostForm("environment"), passedID)
+			context.String(status, id)
+		}
+	})
+
+	router.GET("/:environment/:name", func(context *gin.Context) {
+		status, id := getOrSetID(context.Param("name"), context.Param("environment"), 0)
+		context.String(status, id)
 	})
 
 	router.Run("localhost:8080")
