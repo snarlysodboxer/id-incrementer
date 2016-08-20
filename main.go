ddc47d16fd8ac7ce8444dd278c9b14944287887d
david amick <github@davidamick.com>
2016-08-20 10:56:11 -0700

    Add mutex
    This fixes a race condition with concurrent reads/writes.

--- main.go
+++ main.go
@@ -5,11 +5,15 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strconv"
+	"sync"
 )
+
+// TODO setup auto API documentation, add auth, add persistent storage
 
 var idMap = map[string]map[string]int{}
 var initialValue = 42
 var incrementBy = 5
+var mutex = &sync.Mutex{}
 
 func getOrSetID(name, environment string, passedID int) (int, string) {
 	// check if the environment is found, otherwise add it
@@ -46,11 +50,15 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/list", func(context *gin.Context) {
+		mutex.Lock()
 		context.JSON(http.StatusOK, gin.H{"idMap": idMap})
+		mutex.Unlock()
 	})
 
 	router.GET("/getter/:environment/:name", func(context *gin.Context) {
+		mutex.Lock()
 		status, id := getOrSetID(context.Param("name"), context.Param("environment"), 0)
+		mutex.Unlock()
 		context.String(status, id)
 	})
 
@@ -63,7 +71,9 @@ func main() {
 		if passedID == 0 {
 			context.String(http.StatusBadRequest, "You can't set the ID to zero") // TODO rethink this limitation?
 		} else {
+			mutex.Lock()
 			status, id := getOrSetID(context.PostForm("name"), context.PostForm("environment"), passedID)
+			mutex.Unlock()
 			context.String(status, id)
 		}
 	})
